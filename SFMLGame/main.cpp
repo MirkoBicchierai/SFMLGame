b6799f7db99db77236b801390b871dd6516a0476
MirkoBicchierai <bicchierai.mirko.developer@gmail.com>
2019-07-23 00:33:31 +0200

    hide console when starting game

--- SFMLGame/main.cpp
+++ SFMLGame/main.cpp
@@ -1,7 +1,9 @@
 #include "Game.h"
 #include "ConcreteStateMenu.h"
-
+#include <windows.h>
 int main(){
+    HWND hWnd = GetConsoleWindow();
+    ShowWindow( hWnd, SW_HIDE );
     Game game;
     game.pushState(new ConcreteStateMenu(&game));
     game.gameLoop();
